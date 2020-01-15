3c8b70c6516fd1fb3a4cd83623d2a312c5e9d9ce
jordan <jordan@jordans-MacBook-Air.local>
2020-01-15 20:16:56 +0700

    posts rendering

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const postRouter= require('./posts/postRouter');
 const server = express();
 
+server.use(express.json());
+server.use('/posts', postRouter);
+//server.use('/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
 
 //custom middleware
 
