cbb7cac2ee23ab8eea525e62b4baf47fd85ac142
jordan <jordan@jordans-MacBook-Air.local>
2020-01-15 21:14:22 +0700

    user router done and can get posts of user

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const postRouter= require('./posts/postRouter');
+const express = require("express");
+const postRouter = require("./posts/postRouter");
+const userRouter= require ("./users/userRouter")
 const server = express();
 
 server.use(express.json());
-server.use('/posts', postRouter);
-//server.use('/users', userRouter);
+server.use(logger);
+server.use("/posts", postRouter);
+server.use('/users', userRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
 
+  next();
+}
 
 module.exports = server;
